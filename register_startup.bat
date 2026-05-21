f3f7e5910104222506ac1a95d3f0bbdd794f4b72
CamperInTheWoods <toeuhakim@gmail.com>
2026-05-21 15:35:00 +0900

    Fix: register_startup.bat 한글 경로 인코딩 오류 수정

--- register_startup.bat
+++ register_startup.bat
@@ -1,24 +1,24 @@
 @echo off
-:: 현재 폴더의 recorder.py를 Windows 시작 프로그램에 등록합니다.
+chcp 65001 > nul
 set SCRIPT_PATH=%~dp0recorder.py
 set SHORTCUT_NAME=AutoRecorder
-
-:: 시작 프로그램 폴더에 VBScript로 바로가기 생성
 set STARTUP_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 set VBS_TEMP=%TEMP%\create_shortcut.vbs
 
-echo Set oWS = WScript.CreateObject("WScript.Shell") > "%VBS_TEMP%"
-echo sLinkFile = "%STARTUP_DIR%\%SHORTCUT_NAME%.lnk" >> "%VBS_TEMP%"
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%VBS_TEMP%"
-echo oLink.TargetPath = "pythonw.exe" >> "%VBS_TEMP%"
-echo oLink.Arguments = """%SCRIPT_PATH%""" >> "%VBS_TEMP%"
-echo oLink.WorkingDirectory = "%~dp0" >> "%VBS_TEMP%"
-echo oLink.Description = "Auto Camera Recorder" >> "%VBS_TEMP%"
-echo oLink.Save >> "%VBS_TEMP%"
+(
+echo Set oWS = WScript.CreateObject^("WScript.Shell"^)
+echo sLinkFile = "%STARTUP_DIR%\%SHORTCUT_NAME%.lnk"
+echo Set oLink = oWS.CreateShortcut^(sLinkFile^)
+echo oLink.TargetPath = "pythonw.exe"
+echo oLink.Arguments = """%SCRIPT_PATH%"""
+echo oLink.WorkingDirectory = "%~dp0"
+echo oLink.Description = "Auto Camera Recorder"
+echo oLink.Save
+) > "%VBS_TEMP%"
 
 cscript //nologo "%VBS_TEMP%"
 del "%VBS_TEMP%"
 
-echo [완료] 시작 프로그램에 등록되었습니다: %STARTUP_DIR%\%SHORTCUT_NAME%.lnk
-echo 컴퓨터를 재시작하면 자동으로 실행됩니다.
+echo [완료] 등록됨: %STARTUP_DIR%\%SHORTCUT_NAME%.lnk
+echo 재부팅하면 자동으로 실행됩니다.
 pause
